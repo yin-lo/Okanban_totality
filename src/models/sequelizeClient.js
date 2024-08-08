9631c91cabfa125cb40d783b4e197147a3349b9a
yin-lo <yinlo@hotmail.fr>
2024-08-08 16:16:26 +0200

    modify files but not code

--- src/models/sequelizeClient.js
+++ src/models/sequelizeClient.js
@@ -12,14 +12,4 @@ function getConnexion() {
     });
 }
 
-// export const sequelize =  new Sequelize(process.env.PG_URL, {
-//     define: {
-//         createdAt: 'created_at',
-//         updatedAt: 'updated_at',
-//         underscored: true,
-//     },
-
-//     logging: false,
-// });
-
 export { getConnexion };
